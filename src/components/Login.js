91db603292936b95882db7a0eb42fefaa1501abb
Dennis Padiernos <depadiernos@gmail.com>
2019-11-15 05:09:57 -0600

    create handleSubmit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,31 @@
 import React, { useState } from "react"
+import { LoginUser, setToken } from "../utils/api"
 
 export default function Login() {
   const [credentials, setCredentials] = useState({
     username: "",
     password: ""
   })
+  const [error, setError] = useState()
 
   const onChange = () => {}
 
-  const onSubmit = () => {}
+  const onSubmit = async e => {
+    e.preventDefault()
+    try {
+      const data = await LoginUser(credentials)
+      setToken(data.token)
+    } catch (error) {
+      const status = error.response && error.response.status
+      switch (status) {
+        case 401:
+          setError("Incorrect email and/or password!")
+          break
+        default:
+          setError(error.response)
+      }
+    }
+  }
 
   return (
     <form onSubmit={handleSubmit}>
