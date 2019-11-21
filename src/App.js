85bf72346087f9d8c31d688657a0169aab271780
Dennis Padiernos <depadiernos@gmail.com>
2019-11-21 13:45:06 -0600

    HOTFIX: fix context

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Dashboard from "./components/Dashboard"
 function App() {
   const [user, setUser] = useState()
   return (
-    <UserContext.Provider value={(user, setUser)}>
+    <UserContext.Provider value={{user, setUser}}>
       <div className="App">
         <Route
           exact
