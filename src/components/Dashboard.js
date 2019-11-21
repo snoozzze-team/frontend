46e4c904cd58eb016420d3f3424b44d909b34b20
Dennis Padiernos <depadiernos@gmail.com>
2019-11-21 13:54:42 -0600

    scaffold SleepModal component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,6 +4,7 @@ import { getToken } from "../utils/axiosWithAuth"
 import { UserContext} from '../contexts'
 import Graph from "./Graph"
 import Buttons from "./Input-Sleep"
+import SleepModal from './SleepModal'
 
 export default function Dashboard() {
   const {user, setUser} = useContext(UserContext)
@@ -18,6 +19,7 @@ export default function Dashboard() {
         { user && <h1>Welcome {user.username}</h1>}
       <Graph />
       <Buttons />
+      <SleepModal/>
     </div>
   )
 }
