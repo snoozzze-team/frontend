e4fe39c56b8eff96b7c87de16abba65625ff1461
smv5047 <smv5047@gmail.com>
2019-11-21 20:33:14 -0500

    updated delete function

--- src/components/SleepTable.js
+++ src/components/SleepTable.js
@@ -1,8 +1,10 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 import styled from "styled-components"
 
 import axiosWithAuth from "../utils/axiosWithAuth"
+
+import api from '../utils/axiosWithAuth'
 
 //Styled Components
 
@@ -37,6 +39,7 @@ const SleepTableStyle = styled.div`
 
 function SleepTable (props) {
     const [sleepLog, setSleepLog] = useState([{
+        id: 1,
         userId: '4',
         dateTimeFrom: '2019-10-22T23:00',
         dateTimeTo: '2019-10-23T08:00',
@@ -44,22 +47,53 @@ function SleepTable (props) {
         notes: 'Slept Great -Taran'
     }])
 
+    const [editing, setEditing] = useState(false);
+    const [logToEdit, setLogToEdit] = useState({});
+    const [newTime, setNewTime] = useState(0)
+    const [newMood, setNewMood] = useState({})
 
-    axiosWithAuth().get("/api/users/sleepdata")
-        .then(res=>
-            console.log(res)
-            // setSleepLog(res)
-            )
-        .catch(err=>
-            console.log(err.response)
-            )
+    const editLog = log => {
+        setEditing(true);
+        setLogToEdit(log);
+      };
 
+    useEffect(()=>{
+        axiosWithAuth().get(`/api/users/sleepdata`)
+            .then(res=>
+                console.log(res)
+                // setSleepLog(res.data)
+                )
+            .catch(err=>
+                console.log(err.response)
+                )
+    }, [])
+
+
+    const deleteSleepEntry = sleepEntry =>{
+        axiosWithAuth().delete(`/api/users/sleepdata/${sleepEntry.id}`)
+            .then(res=> {
+                console.log(res)
+                setSleepLog(sleepLog.filter(sleep=>sleep.id !== sleepEntry.id))
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+    }
+
+
+    const updateSleepEntry = sleepEntry =>{
+        axiosWithAuth().put(`/api/users/sleepdata/${sleepEntry.id}`, sleepEntry)
+            .then(res=> {
+                console.log(res)
+                setSleepLog(sleepLog.filter(sleep=>sleep.id !== sleepEntry.id))
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+    }
 
     
     return (
-       
-        //TO-DO - Create filter to filter by User ID so that table only shows logs for the logged in User ID
-        
         <SleepTableStyle>
             <h1>Sleep Table</h1>
             <table>
@@ -70,6 +104,7 @@ function SleepTable (props) {
                         <th>Hours Slept</th>
                         <th>Sleep Score</th>
                         <th>Update</th>
+                        <th>Delete</th>
                     </tr>
                    
 
@@ -77,10 +112,26 @@ function SleepTable (props) {
                         <tr>
                             <td>{log.feels}</td>
                             <td>{log.dateTimeFrom - log.dateTimeTo}</td>
-                    <td>{log.Sleepscore || 'Not Available'}</td>
+                            <td>{log.Sleepscore || 'Not Available'}</td>
                             <td>
                                 <button>Edit</button>
-                                <span> X </span>
+                                {/* {editing && (
+                                    // <form onSubmit={saveEdit}>
+                                    //     {/* <input
+                                    //         placeholder="time slept"
+                                    //         type="text"
+                                    //         name="timeSlept"
+                                    //         value={}
+                                    //         >
+                                    //     </input> */}
+                                    {/* // </form> */}
+                                
+                            </td>
+                            <td>
+                                <span onClick={e=>{
+                                    e.stopPropagation();
+                                    deleteSleepEntry(log)
+                                }}> X </span>
                             </td>
                         </tr>
                     ))}
