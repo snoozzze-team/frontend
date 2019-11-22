04125cc909ea7409d0efde691285b38ebcd3c5f8
smv5047 <smv5047@gmail.com>
2019-11-21 21:53:58 -0500

    date mood and hours slept updated in sleep table

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,8 +3,9 @@ import jwt from "jwt-decode"
 import { getToken } from "../utils/axiosWithAuth"
 import { UserContext } from "../contexts"
 import Graph from "./Graph"
-import Buttons from "./Buttons"
-import SleepModal from "./SleepModal"
+import Buttons from "./Input-Sleep"
+import SleepModal from './SleepModal'
+import SleepTable from './SleepTable'
 
 export default function Dashboard() {
   const { user, setUser } = useContext(UserContext)
@@ -20,7 +21,9 @@ export default function Dashboard() {
       {isAdding && <SleepModal setIsAdding={setIsAdding} userId={user.id}/>}
       {user && <h1>Welcome {user.username}</h1>}
       <Graph />
-      <Buttons isAdding={isAdding} setIsAdding={setIsAdding}/>
+      <Buttons />
+      <SleepTable/>
+      <SleepModal/>
     </div>
   )
 }
