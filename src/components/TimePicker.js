9856ff3f0e1c3f8885969ef1440bbe283bc84ddc
Dennis Padiernos <depadiernos@gmail.com>
2019-11-22 20:00:26 -0600

    make timepicker handle date as well. sends dateTime back.

--- src/components/TimePicker.js
+++ src/components/TimePicker.js
@@ -1,34 +1,56 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import styled from "styled-components"
 import dayjs from "dayjs"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons"
+import DatePicker from "react-date-picker"
+import { SleepContext } from "../contexts"
 
 const Style = styled.div`
   display: flex;
   align-items: center;
-  > div {
+  justify-content: space-evenly;
+  width: 100%;
+  padding: 1rem;
+  .time {
     display: flex;
-    flex-direction: column;
-    max-width: 3rem;
+    align-items: center;
+    > div {
+      display: flex;
+      flex-direction: column;
+    }
+    input {
+      max-width: 2rem;
+    }
+    span {
+      cursor: pointer;
+    }
   }
 `
 
-export default function TimePicker({ onClick }) {
-  const currentTime = dayjs()
+export default function TimePicker({ starting }) {
+  const { setStart, setEnd } = useContext(SleepContext)
+  const [date, setDate] = useState(new Date())
+
   const [time, setTime] = useState({
-    hour: dayjs(currentTime).format("h"),
-    minute: dayjs(currentTime).format("mm"),
-    meridiem: dayjs(currentTime).format("a")
+    hour: dayjs().format("hh"),
+    minute: dayjs().format("mm"),
+    meridiem: dayjs().format("A")
   })
   useEffect(() => {
-    onClick
-      ? onClick(time)
-      : console.log(
-          "TimePicker.js: You need to pass a setting function as `onClick`"
-        )
+    const yearMonthDay = dayjs(date).format("YYYY-MM-DD")
+    const twentyfourhours =
+      time.meridiem === "AM"
+        ? time.hour + ":" + time.minute
+        : parseInt(time.hour) + 12 + ":" + time.minute
+    const completeDate = `${yearMonthDay}T${twentyfourhours}`
+    starting ? setStart(completeDate) : setEnd(completeDate)
     // eslint-disable-next-line
-  }, [])
+  }, [time, date])
+
+  const handleDateChange = date => {
+    setDate(date)
+  }
 
   const handleChange = e => {
     setTime({ ...time, [e.target.name]: e.target.value })
@@ -45,15 +67,11 @@ export default function TimePicker({ onClick }) {
         [part]:
           incrementedTime < 10 ? `0${incrementedTime}` : `${incrementedTime}`
       })
-    } else if (part === "meridiem") {
-      setTime({ ...time, [part]: time[part] === "am" ? "pm" : "am" })
     }
   }
 
   const decrementTime = part => {
     const decrementedTime = parseInt(time[part]) - 1
-    console.log(decrementedTime)
-
     if (part === "hour" && decrementedTime !== 0) {
       setTime({ ...time, [part]: `${decrementedTime}` })
     }
@@ -68,42 +86,52 @@ export default function TimePicker({ onClick }) {
 
   return (
     <Style>
-      <div>
-        <button onClick={() => incrementTime("hour")}>
-          <FontAwesomeIcon color="gray" icon={faChevronUp} />
-        </button>
-        <input
-          type="number"
-          name="hour"
-          min="1"
-          max="12"
-          onChange={handleChange}
-          value={time.hour}
-        />
-        <button onClick={() => decrementTime("hour")}>
-          <FontAwesomeIcon color="gray" icon={faChevronDown} />
-        </button>
+      <div className="date">
+        <DatePicker onChange={handleDateChange} value={date} />
       </div>
-      :
-      <div>
-        <button onClick={() => incrementTime("minute")}>
-          <FontAwesomeIcon color="gray" icon={faChevronUp} />
-        </button>
-        <input
-          type="number"
-          name="minute"
-          min="00"
-          max="59"
-          onChange={handleChange}
-          value={time.minute}
-        />
-        <button onClick={() => decrementTime("minute")}>
-          <FontAwesomeIcon color="gray" icon={faChevronDown} />
-        </button>
+      <div className="time">
+        <div>
+          <button onClick={() => incrementTime("hour")}>
+            <FontAwesomeIcon color="gray" icon={faChevronUp} />
+          </button>
+          <input
+            type="number"
+            name="hour"
+            min="1"
+            max="12"
+            onChange={handleChange}
+            value={time.hour}
+          />
+          <button onClick={() => decrementTime("hour")}>
+            <FontAwesomeIcon color="gray" icon={faChevronDown} />
+          </button>
+        </div>
+        :
+        <div>
+          <button onClick={() => incrementTime("minute")}>
+            <FontAwesomeIcon color="gray" icon={faChevronUp} />
+          </button>
+          <input
+            type="number"
+            name="minute"
+            min="00"
+            max="59"
+            onChange={handleChange}
+            value={time.minute}
+          />
+          <button onClick={() => decrementTime("minute")}>
+            <FontAwesomeIcon color="gray" icon={faChevronDown} />
+          </button>
+        </div>
+        <span
+          name="meridiem"
+          onClick={() =>
+            setTime({ ...time, meridiem: time.meridiem === "AM" ? "PM" : "AM" })
+          }
+        >
+          {time.meridiem}
+        </span>
       </div>
-      <span name="meridiem" onClick={() => incrementTime("meridiem")}>
-        {time.meridiem}
-      </span>
     </Style>
   )
 }
