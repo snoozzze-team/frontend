4fa8c8db1b5123c4ad9ba86aaa72c9a479afd86b
Dennis Padiernos <depadiernos@gmail.com>
2019-11-29 19:08:01 -0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { UserContext } from "./contexts"
 import PrivateRoute from './components/PrivateRoute'
 import AuthModal from "./components/AuthModal"
 import Dashboard from "./components/Dashboard"
+import Navbar from './components/Navbar'
 
 function App() {
   const [user, setUser] = useState()
   return (
     <UserContext.Provider value={{user, setUser}}>
       <div className="App">
+        <Navbar/>
         <Route
           exact
           path="/login"
